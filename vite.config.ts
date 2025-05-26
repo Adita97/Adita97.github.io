17086140ea737e62ef9b020af25ff9d5ef28b85f
marius adrian <marius.adrian97@gmail.com>
2025-05-27 00:51:03 +0300

    deployed for git pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Daniel & Loredana/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
