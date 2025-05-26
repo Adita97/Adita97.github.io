182b8939dd101f70f265e9c34cd952890a715d15
marius adrian <marius.adrian97@gmail.com>
2025-05-27 01:05:46 +0300

    deployed for git pages

--- vite.config.ts
+++ vite.config.ts
@@ -12,8 +12,16 @@ export default defineConfig({
     sourcemap: false,
     rollupOptions: {
       output: {
-        manualChunks: undefined
+        manualChunks: undefined,
+        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js'
       }
     }
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true
   }
 })
